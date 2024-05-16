8bebfc5a102d562139089ab93b970c3886b8cf76
Daphne <aviasdaphne@gmail.com>
2024-05-16 12:06:04 +0200

    ajout exo 8 terminé

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,30 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public class LoginControl extends GridPane {
+    @FXML
+    PasswordField pwd;
+    @FXML
+    TextField ide;
 
+    @FXML
     private void okClicked() {
+        String mdp = "";
+        for (int i=0; i<pwd.getLength(); ++i){
+            mdp += "*";
+        }
+        System.out.println(ide.getText());
+        System.out.println(mdp);
     }
 
+    @FXML
     private void cancelClicked() {
+        ide.clear();
+        pwd.clear();
     }
 }
